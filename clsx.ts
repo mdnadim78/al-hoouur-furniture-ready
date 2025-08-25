200325cb29d507233bcd1bb09266ca87e0857c40
Avro Nill Nadim <mafiatripl@gmail.com>
2025-08-25 02:48:12 -0700

    Clean repo and apply .gitignore

--- clsx.ts
+++
@@ -1,3 +0,0 @@
-export default function clsx(...args: any[]) {
-  return args.filter(Boolean).join(" ");
-}
--- components/ui/separator.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export function Separator() {
-  return <div className="h-px w-full bg-black/10" />;
-}
